602c2b885db76aa9ad789dd86bfb29d8f528c96f
lidachuanginshanghai <13083036055@163.com>
2017-12-06 14:35:02 +0800

    wzx第一次提交

---
+++ src/main/java/com/lee/po/Heart.java
@@ -0,0 +1,25 @@
+package com.lee.po;
+
+/**
+ * Created by root on 2017/12/6.
+ */
+public class Heart {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/lee/po/Welcome.java
@@ -0,0 +1,11 @@
+package com.lee.po;
+
+/**
+ * Created by root on 2017/12/6.
+ */
+public class Welcome {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome!!!!");
+    }
+}
