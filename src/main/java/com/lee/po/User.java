832774c50439454eb69958118475ff09042a1f03
lidachuanginshanghai <13083036055@163.com>
2017-12-06 15:13:07 +0800

    添加用户类的忽略文件

---
+++ src/main/java/com/lee/po/User.java
@@ -0,0 +1,16 @@
+package com.lee.po;
+
+/**
+ * Created by root on 2017/12/6.
+ */
+public class User {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
